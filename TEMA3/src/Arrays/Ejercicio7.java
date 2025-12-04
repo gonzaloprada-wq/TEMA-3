6f599a9bfacd6bf2556833619763ca49d50256a2
Gonzalo <Gonzalo@DESKTOP-76ILFO1>
2025-12-04 18:19:55 +0100

    412254

--- TEMA3/src/Arrays/Ejercicio7.java
+++ TEMA3/src/Arrays/Ejercicio7.java
@@ -1,13 +1,33 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
 
+		int temp []= new int [12];
 		
+		Scanner reader = new Scanner(System.in);
 		
+		for(int i=0; i<12;i++) {
+			
+		System.out.print("Ingresa temperatura para el mes "+(i+1+": "));
+				
+		temp[i]= reader.nextInt();
+							
+		}
 		
+		for(int j=0; j<12;j++) {
+			
+		System.out.print("Temp mes "+(j+1)+": ");
 		
+			for(int k=0;k<100;k++) {
+				
+				if(k<=temp[j]){System.out.print("*");}
+			}
+		System.out.println();
+		}
 		
 	}
 
---
+++ TEMA3/src/Arrays/Ejercicio8.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
