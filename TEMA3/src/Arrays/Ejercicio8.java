153299f67ccf4a589ac39c5d62f0a74a35cfbb7d
Gonzalo <Gonzalo@DESKTOP-76ILFO1>
2025-12-04 19:10:18 +0100

    41225

--- TEMA3/src/Arrays/Ejercicio8.java
+++ TEMA3/src/Arrays/Ejercicio8.java
@@ -1,10 +1,41 @@
 package Arrays;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejercicio8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//asdsadsad
-	}
 
+		int localizar;
+		
+		Scanner reader= new Scanner(System.in);
+		
+		int num[]=new int[100];
+		
+		Random rand = new Random();
+		
+		for(int i=0; i<num.length; i++) {
+			
+			num[i]=rand.nextInt(10)+1;
+			
+		}
+		
+		for(int i=0; i<num.length; i++) {
+			System.out.println();
+			System.out.println("Ingresa numero para localizar: ");
+			
+			localizar = reader.nextInt();
+			
+			System.out.print("Esta en la posicion ");
+			for (int j=0; j<num.length;j++) {
+				
+				if (localizar==num[j]){System.out.print(j+" ");}
+				else if (j==num.length){System.out.println("No se encuentra ese numero");}
+				
+			}
+		}
+		
+		
+	}
 }
